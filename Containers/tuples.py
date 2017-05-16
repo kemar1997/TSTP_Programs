eb68a7ff25103df9ab13eca3960946864dd169a9
kemar1997 <kemar.1997@outlook.com>
2017-05-16 14:13:15 -0400

    Chapter5 - Tuples(Update)

--- Containers/tuples.py
+++ Containers/tuples.py
@@ -19,5 +19,32 @@ To add objects to a tuple, create one with the second syntax with each item you 
 to add, separating them with commas:
 """
 
-rndm = ("M. Jackson", 1958, True)
-print(rndm)
+# rndm = ("M. Jackson", 1958, True)
+# print(rndm)
+
+"""
+Even if a tuple only has one item in it, you need to put a comma after it. That
+way, Python can differentiate it from a number surrounded by parentheses that
+denote its position in the order of operations:
+"""
+
+# this is a tuple
+# my_tuple = ("self_taught",)
+# print(my_tuple)
+
+# this is not a tuple
+# result = (9) + 1
+# print(result)
+
+"""
+You cannot add new item to a tuple or chage it once you've created it. If you try
+to change an object in a tuple after you've created it, Python will raise an 
+exception:
+"""
+
+# dys = ("1984",
+#        "Brave New World",
+#        "Fahrenheit 451")
+
+# dys[1] = "Handmaid's Tale"
+# TypeError: 'tuple' object does not support item assignment
