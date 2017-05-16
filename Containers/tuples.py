f99dfd3a97883156775a2f7feb5d0796e779159b
kemar1997 <kemar.1997@outlook.com>
2017-05-15 23:03:46 -0400

    Chapter5 - Tuples

---
+++ Containers/tuples.py
@@ -0,0 +1,23 @@
+"""
+A tuple is a container that stores objects in a specific order. Unlike lists, tuples
+are immutable, which means their contents cannot change. Once you create a tuple, you
+cannot modify the value of any of the items in it, add new items to it, or remove
+items from it. You represent tuples with parentheses. You must separate items in a
+tuple with commas. There are two syntaxes to create a tuple:
+"""
+
+# my_tuple = tuple()
+# print(my_tuple)
+
+# And:
+
+# my_tuple = ()
+# print(my_tuple)
+
+"""
+To add objects to a tuple, create one with the second syntax with each item you want
+to add, separating them with commas:
+"""
+
+rndm = ("M. Jackson", 1958, True)
+print(rndm)
