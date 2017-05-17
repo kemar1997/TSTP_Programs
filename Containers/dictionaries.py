894b10c5bbbb27547682254db55928793b646bb5
kemar1997 <kemar.1997@outlook.com>
2017-05-17 17:20:28 -0400

    Chapter5 - Dictionaries(First Commit)

---
+++ Containers/dictionaries.py
@@ -0,0 +1,11 @@
+"""
+Dictionaries are another built in container for storing objects. They are used to link one object,
+called a key, to another object called the value. Linking one object to another is called mapping.
+The result is a key-value pair. You add key-value pairs to a dictionary. You can then look up a key
+in the dictionary and get its value. You cannot, however, use a value to look up a key.
+"""
+
+"""
+Dictionaries are mutable, so you can add new key-value pairs to them. Unlike lists and tuples,
+dictionaries do not store objects in a specific order. Their usefulness relies on the 
+"""
