d9b1e99bb5b3b506534beead36040e52b0e16305
kemar1997 <kemar.1997@outlook.com>
2017-05-17 21:01:01 -0400

    Chapter5 - Dictionaries(2nd Commit)

--- Containers/dictionaries.py
+++ Containers/dictionaries.py
@@ -7,5 +7,31 @@ in the dictionary and get its value. You cannot, however, use a value to look up
 
 """
 Dictionaries are mutable, so you can add new key-value pairs to them. Unlike lists and tuples,
-dictionaries do not store objects in a specific order. Their usefulness relies on the 
+dictionaries do not store objects in a specific order. Their usefulness relies on the associations
+formed between keys and values, and there are many situations where you need to store data in pairs.
+For example, you could store information about someone in a dictionary. You could map a key called
+height to a value representing the person's height, a key called eyecolor to a value representing
+the person's eye color and a key called nationality to a value representing the person's
+nationality.
+"""
+
+# Keys          Values
+# 'a'           'alpha'
+# 'o'           'omega'
+# '9'           'gamma'
+
+"""
+Dictionaries are represented with curly brackets. There are two syntaxes for creating dictionaries:
+"""
+
+my_dict = dict()
+print(my_dict)
+
+# And:
+
+my_dict = {}
+print(my_dict)
+
+"""
+You can 
 """
