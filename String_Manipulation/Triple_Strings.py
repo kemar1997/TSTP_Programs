acf24efdcdaad91ef2fff386fcae5b3037385b25
Kemar Golding <kemar.1997@outlook.com>
2017-05-18 14:03:57 -0400

    Chapter 6 - Triple_Strings (1st Commit)

---
+++ String_Manipulation/Triple_Strings.py
@@ -0,0 +1,11 @@
+# If a string spans more than one line, you have to put to put it in triple quotes:
+
+""" line one
+    line two
+    line three
+"""
+
+"""
+If you try to define a string that spans more than one line with single or double quotes, you will get a syntax
+error
+"""
