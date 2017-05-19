aa2aee1a661a99fed256720205b87fca46210d0d
Kemar Golding <kemar.1997@outlook.com>
2017-05-19 03:06:30 -0400

    Chapter 6 - Strings are Immutable (1st Commit)

---
+++ String_Manipulation/strings_are_immutable.py
@@ -0,0 +1,12 @@
+"""
+Strings, like tuples, are immutable. You cannot change the characters in a string. If you want to change the characters
+in a string, you have to create a new string:
+"""
+
+ff = "F. Fitzgerald"
+ff = "F. Scott Fitzgerald"
+print(ff)
+
+"""
+Python has several methods for creating new strings from existing strings.
+"""
