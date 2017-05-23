47aad968dd1b19bcd29b1af558f5731352d526dc
Kemar Golding <kemar.1997@outlook.com>
2017-05-23 09:31:50 -0400

    Chapter 6 - replaces, strip (finished) methods

---
+++ String_Manipulation/replace.py
@@ -0,0 +1,8 @@
+"""
+The replace method replaces every occurrence of a string with another string. The first parameter is the string to
+replace, and the second parameter is the string to replace the occurrence with:
+"""
+
+equ = "All animals are equal."
+equ = equ.replace("l", "0")
+print(equ)
---
+++ String_Manipulation/strip_space.py
@@ -0,0 +1,7 @@
+"""
+You can use the strip method to remove leading and trailing whitespace from a string:
+"""
+
+s = "     The     "
+s = s.strip()
+print(s)
