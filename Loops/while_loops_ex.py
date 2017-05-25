6dd55f4ce3fdfeab5bbe4d9e74694dfd83239be0
Kemar Golding <kemar.1997@outlook.com>
2017-05-25 12:56:00 -0400

    Chapter 7 - While Loops

---
+++ Loops/while_loops_ex.py
@@ -0,0 +1,21 @@
+"""
+While loop: a loop that executes code as long as an expression evaluates to True. The syntax for a while-loop is 
+while [expression]: [code_to_execute], where [expression] represents the expression that determines whether or not the
+loop will continue and [code_to_execute] represents the code the loop should execute as long as it does:
+"""
+
+# x = 10
+# while x > 0:
+#     print('{}'.format(x))
+#     x -= 1
+# print("Happy New Year!!")
+
+"""
+If you define a while-loop with an expression that always evalutes to True, your loop will fun forever. A loop that
+never ends is called an infinite loop. Here is an example of an infinite loop:
+"""
+
+# while True:
+#     print("Hello World!!")
+
+
