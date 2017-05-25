cdce21a0847ffbf21b3e80bce7248cc2e7302f56
Kemar Golding <kemar.1997@outlook.com>
2017-05-25 12:36:56 -0400

    Chapter 7 - Range

---
+++ Loops/range.py
@@ -0,0 +1,17 @@
+"""
+You can use the built-in range function to create a sequence of integers, and use a for-loop to iterate through them.
+The range function takes two parameters: a number where the sequence starts and a number where the sequence stops. The
+sequence of integers returned by the range function includes the first parameter (the number to start at), but not the 
+second parameter (the number to stop at). Here is an example of using the range function to create a sequence of 
+numbers, and iterate through them:
+"""
+
+for i in range(1, 11):
+    print(i)
+
+
+"""
+In this example, you used a for-loop to print each number in the iterable returned by the range function. Programmers
+often name the variable used to iterate through a list of integers i.
+"""
+
