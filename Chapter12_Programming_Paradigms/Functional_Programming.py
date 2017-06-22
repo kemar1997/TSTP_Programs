e3fbfed06772be0bad67de37ca7fd2aa05ace37d
Kemar Golding <Kemar Golding>
2017-06-22 13:44:32 -0400

    continuing learning the various paradigms

--- Chapter12_Programming_Paradigms/Functional_Programming.py
+++ Chapter12_Programming_Paradigms/Functional_Programming.py
@@ -1,3 +1,9 @@
 """
-Functional programming originates from the lambda calculus: the smallest 
+Functional programming originates from the lambda calculus: the smallest universal programming language
+in the world (created by the mathematician Alonzo Church). Functional programming addresses the problems
+that arise in procedural programming by eliminating global state. A functional programmer relies on
+functions that do not use or change global state, the only state they  use are the parameters you pass
+to a function. The result a function returns is usually passed on to another function. A functional
+programmer can thus avoid global state by passing it from function to function. Eliminating global
+state removes side effects and the problems that come with them.
 """
