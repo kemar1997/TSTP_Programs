f9c30b41d354916d82a406d5a32ee6e099c564b2
kemar1997 <kemar.1997@outlook.com>
2017-06-27 21:55:09 -0400

    updating Chapter 13

--- Chapter13_TheFourPillarsOfObjectOrientedProgramming/Encapsulation.py
+++ Chapter13_TheFourPillarsOfObjectOrientedProgramming/Encapsulation.py
@@ -26,13 +26,13 @@ directly accessing it:
 """
 
 
-##class Data:
-##    def __init__(self):
-##        self.nums = [1, 2, 3, 4, 5]
-##
-##
-##    def change_data(self, index, n):
-##        self.nums[index] = n
+# class Data:
+#    def __init__(self):
+#        self.nums = [1, 2, 3, 4, 5]
+
+
+#    def change_data(self, index, n):
+#        self.nums[index] = n
 
 
 """
