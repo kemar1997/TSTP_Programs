2df48404791d100a110b16badfb4272225c936a4
kemar1997 <kemar.1997@outlook.com>
2017-07-29 16:03:48 -0400

    new chapter

--- Chapter9_Files/Writing_to_files.py
+++ Chapter9_Files/Writing_to_files.py
@@ -31,6 +31,25 @@ a few of the modes you can open a file in:
 - "w" opens a file  for writing only. Overwrites the file if the file exists. If the file does not exist, creates a new
 file for writing.
 
-- "w+" opens a file for reading and writing. Overwrites the file if the file exists. If the file does not exist, creates a new
-file for reading and writing.
+- "w+" opens a file for reading and writing. Overwrites the file if the file exists. If the file does not exist, creates
+ a new file for reading and writing.
+
+The open function returns an object, called a file object, which you can use to read and/or write to your file. When you
+use the mode "w", the open function creates a new file, if it doesn't already exist, in the directory your program is
+running in.
+
+You can then use the write method on the file object to write to the file, and the close method to close it. If you open
+a file using the open method, you must close it with the close method. If you use the open method on multiple files and
+forget to close them, it can cause problems in your program. Here is an example of opening a file, writing to it, and
+closing it:
+"""
+
+st = open("st.txt", "w")
+st.write("Hi from Python!")
+st.close()
+
+"""
+In this example, you use the open function to open the file and save the file object it returns in the variable st. Then
+you call the write method on st, which accepts a string as a parameter and writes it to the new file Python created.
+Finally, you close your file by calling the close method on the file object.
 """
